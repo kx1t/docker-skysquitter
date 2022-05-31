ccd52af7a8b9efff28f0273d3970e414866384bd
kx1t <kx1t@amsat.org>
2022-05-31 10:45:38 -0400

    harmonized image names

--- buildnow.sh
+++ buildnow.sh
@@ -8,11 +8,11 @@
 BASETARGET1=ghcr.io/sdr-enthusiasts
 BASETARGET2=kx1t
 
-IMAGE1="$BASETARGET1/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
-IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+#IMAGE1="$BASETARGET1/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
+#IMAGE2="$BASETARGET2/$(pwd | sed -n 's|.*/docker-\(.*\)|\1|p'):$TAG"
 
-[[ "$IMAGE1" == "$BASETARGET1/reversewebproxy:$TAG" ]] && IMAGE1="$BASETARGET1/webproxy:$TAG" || true
-[[ "$IMAGE2" == "$BASETARGET2/reversewebproxy:$TAG" ]] && IMAGE2="$BASETARGET2/webproxy:$TAG" || true
+IMAGE1="$BASETARGET1/$(pwd):$TAG"
+IMAGE2="$BASETARGET2/$(pwd):$TAG"
 
 echo "press enter to start building $IMAGE1 and $IMAGE2 from $BRANCH"
 read
