9457e110f6c98e08b3bbe8534a34a9f59735bfef
kx1t <kx1t@amsat.org>
2022-06-02 18:22:56 -0400

    updated and broadened build platforms

--- buildnow.sh
+++ buildnow.sh
@@ -3,7 +3,7 @@
 
 [[ "$1" != "" ]] && BRANCH="$1" || BRANCH=main
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
-[[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
+[[ "$ARCHS" == "" ]] && ARCHS="linux/386,linux/amd64,linux/arm/v7,linux/arm/v6,linux/arm64"
 
 BASETARGET1=ghcr.io/kx1t
 BASETARGET2=kx1t
